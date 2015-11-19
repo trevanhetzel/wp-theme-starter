dfa016b39140968b1e0a0af2bce77b34fd64154f
Trevan Hetzel <trevan@hetzelcreative.com>
2015-11-19 07:22:15 -0600

    Add webfontloader

--- js/app.js
+++ js/app.js
@@ -6,7 +6,24 @@
 require('picturefill');
 
 /**
+ * Web font loader.
+ */
+var webFont = require('webfontloader');
+
+WebFont.load({
+  google: {
+    families: [
+      'Droid Serif:400',
+      'Droid Serif:400italic',
+      'Droid Serif:700',
+      'Lato:400',
+      'Lato:700'
+    ]
+  }
+});
+
+/**
  * Example module.
  * @see module:exampleModule
  */
-var mainNav = require('./modules/example-module').init();
+var mainNav = require('./module/example-module').init();
---
+++ js/module/web-fonts.js
@@ -0,0 +1,12 @@
+/**
+ * Web fonts.
+ * @module webFonts
+ */
+
+'use strict';
+
+var $ = require('jquery');
+
+module.exports = {
+ 
+}
