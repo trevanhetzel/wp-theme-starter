3fad0ce5bccef51d05d8a7a927072df096d20ce5
Trevan Hetzel <trevan@hetzelcreative.com>
2016-11-30 22:23:56 -0600

    Add favicons and og tags

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Starter theme
 
 My starter WordPress theme. Using Gulp, Browserify, NPM (for dependencies available) and Sass.
+
+### Favicons
+
+The favicon sizes you need are in the root directory (inspired by [https://github.com/audreyr/favicon-cheat-sheet](https://github.com/audreyr/favicon-cheat-sheet)), however you'll need to generate the `favicon.ico` in a special way. Make sure you have ImageMagick installed and then save a 16x16 PNG and 32x32 file in the same directory somewhere. Then run:
+
+`convert favicon-16.png favicon-32.png favicon.ico`
+
+Copy that outputted `favicon.ico` into your theme directory and you're set!
--- header.php
+++ header.php
@@ -5,7 +5,31 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php if(is_front_page() ) { ?> Site Title<?php } ?> <?php wp_title(''); ?> | <?php bloginfo('name'); ?></title>
 
+	<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/favicon-152.png">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon-32.png" sizes="32x32">
+	<link rel="shortcut icon" sizes="196x196" href="<?php echo get_template_directory_uri(); ?>/favicon-196.png">
+	<meta name="msapplication-TileColor" content="#FFFFFF">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/favicon-144.png">
+
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+
+	<?php if (is_front_page()): ?>
+		<meta property="og:title" content="<?php echo get_bloginfo('description'); ?>">
+		<meta name="twitter:title" content="<?php echo get_bloginfo('description'); ?>">
+	<?php else: ?>
+		<meta property="og:title" content="<?php the_title(); ?>">
+		<meta name="twitter:title" content="<?php the_title(); ?>">
+	<?php endif; ?>
+
+	<meta property="og:type" content="article">
+	<meta property="og:url" content="<?php echo "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>">
+	<meta property="og:site_name" content="<?php wp_title(''); ?>">
+	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/og.png">
+	<meta property="og:description" content="<?php echo get_bloginfo('description'); ?>">
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@your_handle">
+	<meta name="twitter:description" content="<?php echo get_bloginfo('description'); ?>">
 
 	<?php wp_head(); ?>
 </head>
