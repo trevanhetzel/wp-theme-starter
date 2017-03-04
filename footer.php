b799e787dfd465e5bea4c83b174a4e9463dd891b
Jon Bellah <jbellah@gmail.com>
2017-03-04 06:43:59 -0700

    Add body class to body tag, remove direct file inclusion and use enqueue

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
-  <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/bundle.js"></script>
-
-  <?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 	</body>
 </html>
--- functions.php
+++ functions.php
@@ -1,13 +1,32 @@
 <?php
 
-	// Remove WP emoji stuff
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+// Remove WP emoji stuff
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
-	// Custom post types
-	require get_template_directory() . '/inc/post-types.php';
+// Custom post types
+require get_template_directory() . '/inc/post-types.php';
 
-	// Enable post thumbnails
-	require get_template_directory() . '/inc/thumbnails.php';
+// Enable post thumbnails
+require get_template_directory() . '/inc/thumbnails.php';
 
-?>
+function starter_scripts() {
+	wp_enqueue_script(
+		'starter-bundle',
+		esc_url( get_template_directory_uri() ) . '/bundle.js',
+		array(),
+		'0.1',
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'starter_scripts' );
+
+function starter_styles() {
+	wp_enqueue_style(
+		'starter-styles',
+		esc_url( get_template_directory_uri() ) . '/style.css',
+		array(),
+		'0.1'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'starter_styles' );
